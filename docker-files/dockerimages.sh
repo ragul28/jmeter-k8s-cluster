757f7c8be87649f5b4a3fcb0754b81d0e4caefff
Ragulan <ragulh28@gmail.com>
2023-12-28 17:10:01 +0000

    Move the base image to ghcr

--- docker-files/dockerimages.sh
+++ docker-files/dockerimages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-docker build --tag="kubernautslabs/jmeter-base:latest" -f Dockerfile-base .
-docker build --tag="kubernautslabs/jmeter-master:latest" -f Dockerfile-master .
-docker build --tag="kubernautslabs/jmeter-slave:latest" -f Dockerfile-slave .
-docker build --tag="kubernautslabs/jmeter-reporter" -f Dockerfile-reporter .
+docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-bash:latest" -f Dockerfile-base .
+docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-master:latest" -f Dockerfile-master .
+docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-slave:latest" -f Dockerfile-slave .
+docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-reporter" -f Dockerfile-reporter .
