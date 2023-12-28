6d6803feaee338f792bc70762c99328e5ab07265
Ragulan <ragulh28@gmail.com>
2023-12-28 17:11:29 +0000

    Replace grafana reporter image with maintainer version

--- docker-files/dockerimages.sh
+++ docker-files/dockerimages.sh
@@ -3,4 +3,3 @@
 docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-bash:latest" -f Dockerfile-base .
 docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-master:latest" -f Dockerfile-master .
 docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-slave:latest" -f Dockerfile-slave .
-docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-reporter" -f Dockerfile-reporter .
