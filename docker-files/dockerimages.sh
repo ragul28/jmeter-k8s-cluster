6380198553a67c189956cf65d4ca035c7bb0b608
Ragulan <ragulh28@gmail.com>
2023-12-28 17:13:12 +0000

    Update the Dockerfile names to standerd format

--- docker-files/dockerimages.sh
+++ docker-files/dockerimages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-bash:latest" -f Dockerfile-base .
-docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-master:latest" -f Dockerfile-master .
-docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-slave:latest" -f Dockerfile-slave .
+docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-bash:latest" -f base.Dockerfile .
+docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-master:latest" -f master.Dockerfile .
+docker build --tag="ghcr.io/ragul28/jmeter-k8s-cluster/jmeter-slave:latest" -f slave.Dockerfile .
