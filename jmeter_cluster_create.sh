736d88c4c820613c55dbc3b536cbf0160c0e23d7
Ragulan <ragulh28@gmail.com>
2023-12-29 10:22:32 +0000

    Fix create script support kube 1.27+

--- jmeter_cluster_create.sh
+++ jmeter_cluster_create.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#Create multiple Jmeter namespaces on an existing kuberntes cluster
+#Create Jmeter cluster on existing kuberntes cluster namespace
 
 working_dir=`pwd`
 
@@ -12,7 +12,7 @@ then
     exit
 fi
 
-kubectl version --short
+kubectl version --client
 
 echo "Enter the namespace [default]:"
 read tenant
@@ -20,36 +20,34 @@ read tenant
 if [[ -z $tenant ]]
 then
     tenant=default
+    echo "Using $tenant namespace"
+else
+    kubectl create ns $tenant
+    echo "Using $tenant namespace has been created"
 fi
 
-echo "Using $tenant namespace has been created"
 echo
-
 echo "Creating Jmeter slave nodes"
-nodes=`kubectl get no | egrep -v "master|NAME" | wc -l`
+nodes=`kubectl get no | egrep -v "master|control|NAME" | wc -l`
 
 echo "Number of worker nodes on this cluster is " $nodes
 
-echo "Enter the number of jmeter-slaves [$nodes]:"
+echo "Enter the number of jmeter-slaves [$((nodes - 1))]:"
 read num_slaves
 
 if [[ -z $num_slaves ]]
 then
-    num_slaves=$nodes
+    num_slaves=$((nodes - 1))
 fi
+
 echo "Creating $num_slaves Jmeter slave replicas and service"
-echo
-
 sed -i "s~^\([[:blank:]]*\)replicas:.*$~\1replicas: $num_slaves~" $working_dir/jmeter-cluster/jmeter_slaves_deploy.yaml
-
 kubectl create -n $tenant -f $working_dir/jmeter-cluster/jmeter_slaves_deploy.yaml
-
 kubectl create -n $tenant -f $working_dir/jmeter-cluster/jmeter_slaves_svc.yaml
 
+echo
 echo "Creating Jmeter Master"
-
 kubectl create -n $tenant -f $working_dir/jmeter-cluster/jmeter_master_configmap.yaml
-
 kubectl create -n $tenant -f $working_dir/jmeter-cluster/jmeter_master_deploy.yaml
 
 
@@ -57,4 +55,4 @@ echo "Printout Of the $tenant Objects"
 echo
 kubectl get -n $tenant all
 
-echo namespace = $tenant > $working_dir/jmeter-cluster/tenant_export
+echo namespace = $tenant > $working_dir/tenant_export
